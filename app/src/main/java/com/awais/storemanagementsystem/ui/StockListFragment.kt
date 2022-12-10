82327ceca0c5655f849412167097cd1559a7d3d9
Awais1097 <awaisbagga1097@gmail.com>
2022-12-10 15:54:52 +0500

    working on stock

---
+++ app/src/main/java/com/awais/storemanagementsystem/ui/StockListFragment.kt
@@ -0,0 +1,40 @@
+package com.awais.storemanagementsystem.ui
+
+import androidx.lifecycle.ViewModelProvider
+import com.awais.storemanagementsystem.adapter.ProductsListAdapter
+import com.awais.storemanagementsystem.roomdb.entity.ProductEntity
+import com.awais.storemanagementsystem.viewmodel.ProductsViewModel
+
+class StockListFragment : BaseListFragment() {
+
+
+    override fun loadData() {
+        val viewModel =
+            ViewModelProvider(this)[ProductsViewModel::class.java]
+        viewModel.getAllProducts()
+        viewModel.list.observe(viewLifecycleOwner) {
+            binding.list.adapter = ProductsListAdapter(it,
+                onItemClick = object : ProductsListAdapter.OnItemClick {
+
+                    override fun onDelete(item: ProductEntity) {
+
+                    }
+
+                    override fun onClick(item: ProductEntity) {
+
+                    }
+                }
+            )
+        }
+    }
+
+    override fun showFab(): Boolean {
+        return false
+    }
+
+    companion object {
+        val TAG: String = StockListFragment::class.java.name
+    }
+
+
+}
